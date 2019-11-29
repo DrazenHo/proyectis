bc4f54418fa27532215241dee2a4c5532a6c601f
DrazenHo <54466679+DrazenHo@users.noreply.github.com>
2019-11-29 01:05:51 -0600

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,76 @@
+function Login(){ 
+var done=0; 
+var usuario=document.login.usuario.value; 
+var password=document.login.password.value; 
+    
+if (usuario=="suc768" && password=="suc768") { 
+window.location="redireccionamiento.html"; 
+} 
+   
+if (usuario=="suc769" && password=="suc769") { 
+window.location="inicio.html"; 
+} 
+    
+    
+if (usuario=="suc771" && password=="suc771") { 
+window.location="suc772/redireccionamiento.html"; 
+} 
+ 
+if (usuario=="suc772" && password=="suc772") { 
+window.location="suc772/rd3.html";
+}   
+
+if (usuario=="suc776" && password=="suc776") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="suc780" && password=="suc780") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="suc1380" && password=="suc1380") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="suc3314" && password=="suc3314") { 
+window.location="redireccionamiento.html"; 
+}    
+
+if (usuario=="suc3320" && password=="suc3320") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="enrique.mar@pitacuchi.com" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+    
+if (usuario=="juan.mar@pitacuchi.com" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="sanjuana_aguilar@pitacuchi.com" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="janette_rodriguez@pitacuchi.com" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="adan_hernandez@pitacuchi.com" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}    
+    
+if (usuario=="enrique.mar" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+
+if (usuario=="suc1601" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+ 
+if (usuario=="acer" && password=="acceso") { 
+window.location="redireccionamiento.html"; 
+}
+    
+
+}
